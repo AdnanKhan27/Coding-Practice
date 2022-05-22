8727068a99461d26bb768e2c67de5eae72bd220e
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-05-22 14:56:40 +0530

    added the python practice folder

---
+++ Python/basics.py
@@ -0,0 +1,14 @@
+
+```python3
+# This is a Single Line comment starting with (hash #)
+
+'''
+This is a multi-line comment wrapped in 3 single/double quotes
+'''
+
+It's a good practice to use as much *useful* comment in your code 
+
+# Printing Stuff
+print("Hello World!") 
+
+```
