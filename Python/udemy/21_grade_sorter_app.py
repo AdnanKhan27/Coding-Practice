fc73e258eb1c0b5b5b85f338afa6d88b8a4956ad
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-07-19 23:48:06 +0530

    completed grade sorter app

--- Python/udemy/21_grade_sorter_app.py
+++ Python/udemy/21_grade_sorter_app.py
@@ -13,3 +13,26 @@ third_grade = int(input("What is your third grade (0-100): "))
 fourth_grade = int(input("What is your fourth grade (0-100): "))
 
 grades.append(first_grade)
+grades.append(second_grade)
+grades.append(third_grade)
+grades.append(fourth_grade)
+
+# Printing Grades
+print("Your grades are: ",grades)
+
+# Sorting grades in descending order
+grades.sort()
+grades.reverse()
+
+print("Your grades from highest to lowest are: ",grades)
+
+# Finding the lowest grades
+print("The lowest two grades will now be dropped.")
+lowest_1 = grades.pop(-1)
+lowest_2 = grades.pop(-2)
+
+print("Removed grade: ",lowest_1)
+print("Removed grade: ",lowest_2)
+
+print("Your remaining grades are: ",grades)
+print("Nice work! Your highest grade is a ",grades[0])
