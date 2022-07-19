3790617cbdd06737f7d6460ec4aa41b56ea85ad5
Adnan Khan <adnan.khan2019@vitbhopal.ac.in>
2022-07-19 21:14:07 +0530

    added grade sorter app file

--- Python/udemy/21_grade_sorter_app.py
+++ Python/udemy/21_grade_sorter_app.py
@@ -1,1 +1,15 @@
 # Lists Challenge 6: Grade Sorter App
+# Use append() to add elements to a list
+# OR use list.extend() to add multiple elements to a list
+
+print("Welcome to the Grade Sorter App 📑")
+
+grades = []
+
+# Getting user input
+first_grade = int(input("What is your first grade (0-100): "))
+second_grade = int(input("What is your second grade (0-100): "))
+third_grade = int(input("What is your third grade (0-100): "))
+fourth_grade = int(input("What is your fourth grade (0-100): "))
+
+grades.append(first_grade)
